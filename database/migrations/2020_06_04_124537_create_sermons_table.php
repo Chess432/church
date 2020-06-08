25b26164396506f6042580ee187e0200d5e09034
Steve <mbathawakawea@gmail.com>
2020-06-08 11:00:08 +0300

    Made first comit for all files

---
+++ database/migrations/2020_06_04_124537_create_sermons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSermonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sermons', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('subject');
+            $table->string('text');
+            $table->string('Speaker');
+            $table->string('date');
+            $table->text('scripture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sermons');
+    }
+}
---
+++ database/migrations/2020_06_08_075148_create_programs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('photo')->default('');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')
+            ->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programs');
+    }
+}
